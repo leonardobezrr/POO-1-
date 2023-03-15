8857ee3851653914eb06ed680b14ecb3de036881
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-03-14 23:09:01 -0300

    ok

--- atividade1.dart
+++ atividade1.dart
@@ -10,7 +10,19 @@ void valores() {
   var item2 = Item(quantidade: 20, produto: prod2);
   var item3 = Item(quantidade: 10, produto: prod3);
   var venda = Venda(data: "09/03/2020", itens: [item, item2, item3]);
-  print("Valor total da venda: ");
+
+  print("Produtos da venda: \n");
+  venda.itens.forEach((itemVenda) {
+    print(itemVenda.quantidade.toString() +
+        " " +
+        itemVenda.produto.descricao +
+        ": R\$" +
+        itemVenda.produto.preco.toString() +
+        " unidade");
+    print("Total produto: "+ (itemVenda.quantidade * itemVenda.produto.preco).toString() + "\n" );
+  });
+
+  print("\nValor total da venda: ");
   print(venda.total());
 }
 
@@ -26,7 +38,7 @@ class Produto {
 }
 
 class Item {
-  double quantidade;
+  int quantidade;
   Produto produto;
 
   double total() => quantidade * produto.preco;
@@ -46,6 +58,7 @@ class Venda {
       value.quantidade += element.quantidade;
       return value;
     });
+    
     return res.total();
   }
 
