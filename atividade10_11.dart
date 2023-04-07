421667ef46d5734a2c2417ee4dfd62a6c2fa7471
Leonardo Bezerra <102527664+leonardobezrr@users.noreply.github.com>
2023-04-07 12:23:31 -0300

    ok

--- atividade10_11.dart
+++ atividade10_11.dart
@@ -92,6 +92,104 @@ class Tabela extends StatelessWidget {
               DataCell(Text('10')),
             ],
           ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Heineken')),
+              DataCell(Text('Pilsen morgob')),
+              DataCell(Text('30')),
+            ],
+          ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Ouro Branco')),
+              DataCell(Text('White Gold')),
+              DataCell(Text('79')),
+            ],
+          ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Black bows')),
+              DataCell(Text('Dart incense')),
+              DataCell(Text('28')),
+            ],
+          ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Budweiser')),
+              DataCell(Text('Headache')),
+              DataCell(Text('5')),
+            ],
+          ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Stella')),
+              DataCell(Text('Standard American Expensive')),
+              DataCell(Text('32')),
+            ],
+          ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Brahma Puro Malte')),
+              DataCell(Text('Pilsen')),
+              DataCell(Text('24')),
+            ],
+          ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Nilsin')),
+              DataCell(Text('Pirsen')),
+              DataCell(Text('54')),
+            ],
+          ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Gold')),
+              DataCell(Text('Extravagance')),
+              DataCell(Text('33')),
+            ],
+          ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Thin Bottle')),
+              DataCell(Text('Fat')),
+              DataCell(Text('13')),
+            ],
+          ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Green Life')),
+              DataCell(Text('PL')),
+              DataCell(Text('22')),
+            ],
+          ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Puro Malte')),
+              DataCell(Text('Pilsen')),
+              DataCell(Text('66')),
+            ],
+          ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Thorne Malta')),
+              DataCell(Text('Yango Bal')),
+              DataCell(Text('44')),
+            ],
+          ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Posthurne')),
+              DataCell(Text('Pirsen')),
+              DataCell(Text('36')),
+            ],
+          ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Quirathi')),
+              DataCell(Text('Malsarrar')),
+              DataCell(Text('54')),
+            ],
+          ),
         ],
       ),
     );
