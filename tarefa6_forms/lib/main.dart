d565771b6cdece691cdf7932cdcc67a955fb40f5
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-04-24 22:57:17 -0300

    qse ok

--- tarefa6_forms/lib/main.dart
+++ tarefa6_forms/lib/main.dart
@@ -21,7 +21,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Create a Form widget.
 class MyCustomForm extends StatefulWidget {
   const MyCustomForm({super.key});
 
@@ -31,22 +30,17 @@ class MyCustomForm extends StatefulWidget {
   }
 }
 
-// Create a corresponding State class.
-// This class holds data related to the form.
+
 class MyCustomFormState extends State<MyCustomForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a GlobalKey<FormState>,
-  // not a GlobalKey<MyCustomFormState>.
+  
   final _formKey = GlobalKey<FormState>();
 
   String _name = '';
   String _email = '';
+  String? _gender;
 
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
     return Form(
       key: _formKey,
       child: Padding(
@@ -58,7 +52,6 @@ class MyCustomFormState extends State<MyCustomForm> {
               decoration: const InputDecoration(
                 labelText: 'Nome',
               ),
-              // The validator receives the text that the user has entered.
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return 'Por favor, digite seu nome.';
@@ -69,11 +62,11 @@ class MyCustomFormState extends State<MyCustomForm> {
                 _name = value!;
               },
             ),
+            
             TextFormField(
               decoration: const InputDecoration(
                 labelText: 'Email',
               ),
-              // The validator receives the text that the user has entered.
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return 'Por favor, digite seu email.';
@@ -87,14 +80,14 @@ class MyCustomFormState extends State<MyCustomForm> {
                 _email = value!;
               },
             ),
+
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: ElevatedButton(
                 onPressed: () {
-                  // Validate returns true if the form is valid, or false otherwise.
+                  
                   if (_formKey.currentState!.validate()) {
-                    // If the form is valid, save the form fields and display a snackbar. In the real world,
-                    // you'd often call a server or save the information in a database.
+                    
                     _formKey.currentState!.save();
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(content: Text('Processando dados...')),
@@ -105,6 +98,38 @@ class MyCustomFormState extends State<MyCustomForm> {
                 child: const Text('Enviar'),
               ),
             ),
+            
+            RadioListTile(
+              title: const Text('Masculino'),
+              value: 'Masculino',
+              groupValue: _gender,
+              onChanged: (value) {
+                setState(() {
+                  _gender = value as String?;
+                });
+              },
+            ),
+            RadioListTile(
+              title: const Text('Feminino'),
+              value: 'Feminino',
+              groupValue: _gender,
+              onChanged: (value) {
+                setState(() {
+                  _gender = value as String?;
+                });
+              },
+            ),
+            RadioListTile(
+              title: const Text('Gilete'),
+              value: 'Gilete',
+              groupValue: _gender,
+              onChanged: (value) {
+                setState(() {
+                  _gender = value as String?;
+                });
+              },
+            ),
+
           ],
         ),
       ),
