7d3ea13d8583d31cc5ddb4ea220d70b345a6072f
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-04-28 19:09:09 -0300

    qse ok
    Botão muda de cor quando seleciona

--- receita5/lib/main.dart
+++ receita5/lib/main.dart
@@ -9,20 +9,18 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      debugShowCheckedModeBanner:false,
-      home: Scaffold(
-        appBar: AppBar( 
-          title: const Text("Dicas"),
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Dicas"),
           ),
-        body: DataTableWidget(jsonObjects:dataObjects),
-        bottomNavigationBar: NewNavBar(),
-      ));
+          body: DataTableWidget(jsonObjects: dataObjects),
+          bottomNavigationBar: NewNavBar(),
+        ));
   }
 }
 
@@ -35,44 +33,45 @@ class NewNavBar extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    var state = useState(0);
     return BottomNavigationBar(
-      onTap: buttonTapped, 
-      currentIndex: 2,
-      items: const [
-      BottomNavigationBarItem(
-        label: "Cafés",
-        icon: Icon(Icons.coffee_outlined),
-      ),
-      BottomNavigationBarItem(
-          label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-      BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))
-    ]);
+        onTap: (index) {
+          state.value = index;
+        },
+        currentIndex: state.value,
+        items: const [
+          BottomNavigationBarItem(
+            label: "Cafés",
+            icon: Icon(Icons.coffee_outlined),
+          ),
+          BottomNavigationBarItem(
+              label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
+          BottomNavigationBarItem(
+              label: "Nações", icon: Icon(Icons.flag_outlined))
+        ]);
   }
 }
 
 class DataTableWidget extends StatelessWidget {
   final List jsonObjects;
-  DataTableWidget( {this.jsonObjects = const [] });
+  DataTableWidget({this.jsonObjects = const []});
 
   @override
   Widget build(BuildContext context) {
-    var columnNames = ["Nome","Estilo","IBU"],
+    var columnNames = ["Nome", "Estilo", "IBU"],
         propertyNames = ["name", "style", "ibu"];
     return DataTable(
-      columns: columnNames.map( 
-                (name) => DataColumn(
-                  label: Expanded(
-                    child: Text(name, style: TextStyle(fontStyle: FontStyle.italic))
-                  )
-                )
-              ).toList()
-      ,       
-      rows: jsonObjects.map( 
-        (obj) => DataRow(
-            cells: propertyNames.map(
-              (propName) => DataCell(Text(obj[propName]))
-            ).toList()
-          )
-        ).toList());
+        columns: columnNames
+            .map((name) => DataColumn(
+                label: Expanded(
+                    child: Text(name,
+                        style: TextStyle(fontStyle: FontStyle.italic)))))
+            .toList(),
+        rows: jsonObjects
+            .map((obj) => DataRow(
+                cells: propertyNames
+                    .map((propName) => DataCell(Text(obj[propName])))
+                    .toList()))
+            .toList());
   }
 }
