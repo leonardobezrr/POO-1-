6a0d3c4f502494957a1f47285ef5ad51ace73970
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-05-09 09:06:42 -0300

    ok

--- receita6/lib/main.dart
+++ receita6/lib/main.dart
@@ -41,6 +41,7 @@ class DataService {
       {"name": "La Fin Du Monde", "style": "Bock", "ibu": "65"},
       {"name": "Sapporo Premiume", "style": "Sour Ale", "ibu": "54"},
       {"name": "Duvel", "style": "Pilsner", "ibu": "82"}
+      
     ];
   }
 
