c9eecb1462e8635f6c47bb4838039cc15917788e
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-05-15 16:21:51 -0300

    qse ok

--- receita7/lib/main.dart
+++ receita7/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:http/http.dart' as http;
 
 class DataService{
   final ValueNotifier<List> tableStateNotifier = new ValueNotifier([]);
