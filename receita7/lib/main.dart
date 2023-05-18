8ff0ac2d53997d355e7be5fc0626624e8950c6f7
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-05-18 15:07:47 -0300

    carregar cervejas ok - fazer cafés e nações agora

--- receita7/lib/main.dart
+++ receita7/lib/main.dart
@@ -3,16 +3,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-
-
-class DataService{
-
+class DataService {
   final ValueNotifier<List> tableStateNotifier = ValueNotifier([]);
 
-  
-
-  void carregar(index){
-
+  void carregar(index) {
     var res = null;
 
     print('carregar #1 - antes de carregarCervejas');
@@ -20,22 +14,14 @@ class DataService{
     if (index == 1) res = carregarCervejas();
 
     print('carregar #2 - carregarCervejas retornou $res');
-
   }
 
-
-
-  Future<void> carregarCervejas() async{
-
+  Future<void> carregarCervejas() async {
     var beersUri = Uri(
-
-      scheme: 'https',
-
-      host: 'random-data-api.com',
-
-      path: 'api/beer/random_beer',
-
-      queryParameters: {'size': '5'});
+        scheme: 'https',
+        host: 'random-data-api.com',
+        path: 'api/beer/random_beer',
+        queryParameters: {'size': '20'});
 
     print('carregarCervejas #1 - antes do await');
 
@@ -45,212 +31,107 @@ class DataService{
 
     var beersJson = jsonDecode(jsonString);
 
-
-
     tableStateNotifier.value = beersJson;
-
   }
-
 }
-
-
 
 final dataService = DataService();
 
-
-
-
-
 void main() {
-
   MyApp app = MyApp();
 
   runApp(app);
-
 }
-
-
 
 class MyApp extends StatelessWidget {
-
-
-
   @override
-
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-
-      debugShowCheckedModeBanner:false,
-
-      home: Scaffold(
-
-        appBar: AppBar( 
-
-          title: const Text("Dicas"),
-
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Dicas"),
           ),
+          body: ValueListenableBuilder(
+              valueListenable: dataService.tableStateNotifier,
+              builder: (_, value, __) {
+                return ListView(
+                  children: [
+                    DataTableWidget(
 
-        body: ValueListenableBuilder(
+                    jsonObjects:value, 
 
-          valueListenable: dataService.tableStateNotifier,
+                    propertyNames: ["name","style","ibu"], 
 
-          builder:(_, value, __){
+                    columnNames: ["Nome", "Estilo", "IBU"]
 
-            return DataTableWidget(
-
-              jsonObjects:value, 
-
-              propertyNames: ["name","style","ibu"], 
-
-              columnNames: ["Nome", "Estilo", "IBU"]
-
-            );
-
-          }
-
-        ),
-
-        bottomNavigationBar: NewNavBar(itemSelectedCallback: dataService.carregar),
-
-      ));
-
+                  )
+                  ]
+                );
+              }),
+          bottomNavigationBar:
+              NewNavBar(itemSelectedCallback: dataService.carregar),
+        ));
   }
-
-
-
 }
-
-
-
-
 
 class NewNavBar extends HookWidget {
-
   final _itemSelectedCallback;
 
-  NewNavBar({itemSelectedCallback}):
-
-    _itemSelectedCallback = itemSelectedCallback ?? (int){}
-
-    
-
-
+  NewNavBar({itemSelectedCallback})
+      : _itemSelectedCallback = itemSelectedCallback ?? (int) {}
 
   @override
-
   Widget build(BuildContext context) {
-
     var state = useState(1);
 
-    
-
     return BottomNavigationBar(
+        onTap: (index) {
+          state.value = index;
 
-      onTap: (index){
-
-        state.value = index;
-
-        _itemSelectedCallback(index);                
-
-      }, 
-
-      currentIndex: state.value,
-
-      items: const [
-
-        BottomNavigationBarItem(
-
-          label: "Cafés",
-
-          icon: Icon(Icons.coffee_outlined),
-
-        ),
-
-
-
-        BottomNavigationBarItem(
-
-            label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-
-
-
-        BottomNavigationBarItem(
-
-          label: "Nações", icon: Icon(Icons.flag_outlined))
-
-      ]);
-
-
-
+          _itemSelectedCallback(index);
+        },
+        currentIndex: state.value,
+        items: const [
+          BottomNavigationBarItem(
+            label: "Cafés",
+            icon: Icon(Icons.coffee_outlined),
+          ),
+          BottomNavigationBarItem(
+              label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
+          BottomNavigationBarItem(
+              label: "Nações", icon: Icon(Icons.flag_outlined))
+        ]);
   }
-
-
-
 }
 
-
-
-
-
-
-
 class DataTableWidget extends StatelessWidget {
-
-
-
   final List jsonObjects;
 
   final List<String> columnNames;
 
   final List<String> propertyNames;
 
-
-
-  DataTableWidget( {this.jsonObjects = const [], this.columnNames = const ["Nome","Estilo","IBU"], this.propertyNames= const ["name", "style", "ibu"]});
-
-
+  DataTableWidget(
+      {this.jsonObjects = const [],
+      this.columnNames = const ["Nome", "Estilo", "IBU"],
+      this.propertyNames = const ["name", "style", "ibu"]});
 
   @override
-
   Widget build(BuildContext context) {
-
     return DataTable(
-
-      columns: columnNames.map( 
-
-                (name) => DataColumn(
-
-                  label: Expanded(
-
-                    child: Text(name, style: TextStyle(fontStyle: FontStyle.italic))
-
-                  )
-
-                )
-
-              ).toList()       
-
-      ,
-
-      rows: jsonObjects.map( 
-
-        (obj) => DataRow(
-
-            cells: propertyNames.map(
-
-              (propName) => DataCell(Text(obj[propName]))
-
-            ).toList()
-
-          )
-
-        ).toList());
-
+        columns: columnNames
+            .map((name) => DataColumn(
+                label: Expanded(
+                    child: Text(name,
+                        style: TextStyle(fontStyle: FontStyle.italic)))))
+            .toList(),
+        rows: jsonObjects
+            .map((obj) => DataRow(
+                cells: propertyNames
+                    .map((propName) => DataCell(Text(obj[propName])))
+                    .toList()))
+            .toList());
   }
-
-
-
 }
