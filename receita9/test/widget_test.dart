a3ada841d870ff8ccf4086a2365a70e3ae231d8a
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-06-14 14:53:23 -0300

    feat: correcting

--- receita9/test/widget_test.dart
+++ receita9/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:receita9/main.dart';
+import 'package:receita9/view/widgets.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
