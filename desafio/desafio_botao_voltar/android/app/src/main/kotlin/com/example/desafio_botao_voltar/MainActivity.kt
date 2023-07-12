d2f37a8c7088db31564f5e63559613792bfc2614
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-07-12 16:21:18 -0300

    feat: falta implementar a ação do restart

---
+++ desafio/desafio_botao_voltar/android/app/src/main/kotlin/com/example/desafio_botao_voltar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.desafio_botao_voltar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ desafio/desafio_botao_voltar/lib/util/ordenador.dart
@@ -0,0 +1,32 @@
+import 'decididor.dart';
+
+class Ordenador {
+  List ordenarItem(List item, Decididor decididor) {
+    List itemOrdenadas = List.of(item);
+
+    bool trocouAoMenosUm;
+
+    do {
+      trocouAoMenosUm = false;
+
+      for (int i = 0; i < itemOrdenadas.length - 1; i++) {
+        var atual = itemOrdenadas[i];
+
+        var proximo = itemOrdenadas[i + 1];
+
+        //Feito com ajuda de Ricardo
+        if (decididor.precisaTrocarAtualPeloProximo(atual, proximo)) {
+          var aux = itemOrdenadas[i];
+
+          itemOrdenadas[i] = itemOrdenadas[i + 1];
+
+          itemOrdenadas[i + 1] = aux;
+
+          trocouAoMenosUm = true;
+        }
+      }
+    } while (trocouAoMenosUm);
+
+    return itemOrdenadas;
+  }
+}
