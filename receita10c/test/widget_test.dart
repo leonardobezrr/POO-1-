6788119fe3bc3a708a09eed80545cf12e945ef88
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-07-17 11:55:26 -0300

    feat: ok

--- receita10c/test/widget_test.dart
+++ receita10c/test/widget_test.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:receita10c/view/widgets.dart';
 
 import 'package:receita10c/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
 
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( MyApp());
 
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
